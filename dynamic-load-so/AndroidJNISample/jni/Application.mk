48659e2aff701b6acac39f6456d22313e58a41b6
Kaede <kidhaibara@gmail.com>
2015-11-26 14:28:46 +0800

    add jni source files

---
+++ dynamic-load-so/AndroidJNISample/jni/Application.mk
@@ -0,0 +1,8 @@
+STLPORT_FORCE_REBUILD := false
+APP_STL := stlport_static
+APP_CPPFLAGS += -fno-exceptions
+APP_CPPFLAGS += -fno-rtti
+GLOBAL_CFLAGS =   -fvisibility=hidden
+APP_ABI := armeabi-v7a
+
+APP_PLATFORM := android-9
