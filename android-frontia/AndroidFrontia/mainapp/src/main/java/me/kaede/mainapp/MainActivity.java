e2930695547080e9d6ece5d168edc3d9bd4e59e7
Kaede <kidhaibara@gmail.com>
2015-12-08 22:21:15 +0800

    update Frontia

--- android-frontia/AndroidFrontia/mainapp/src/main/java/me/kaede/mainapp/MainActivity.java
+++ android-frontia/AndroidFrontia/mainapp/src/main/java/me/kaede/mainapp/MainActivity.java
@@ -1,4 +1,4 @@
-package me.kaede.androidfrontiamainapp;
+package me.kaede.mainapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- android-frontia/AndroidFrontia/pluginapp1/src/main/java/me/kaede/pluginapp1/MainActivity.java
+++ android-frontia/AndroidFrontia/pluginapp1/src/main/java/me/kaede/pluginapp1/MainActivity.java
@@ -2,11 +2,12 @@ package me.kaede.pluginapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.yy.mobile.ylink.dynamicload.DLBasePluginActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DLBasePluginActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
